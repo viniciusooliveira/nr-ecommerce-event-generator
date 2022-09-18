a3f4d17ffd839e11f058ed530e4a0063bbac7072
Vinícius Oliveira <eu@viniciusoliveira.me>
2022-09-18 17:38:36 -0300

    Add initial project and new relic metrics

---
+++ src/domain/enums.ts
@@ -0,0 +1,6 @@
+export enum PaymentMethod {
+    CREDIT_CARD = 'CREDIT CARD',
+    PIX = 'PIX',
+    DEBIT_CARD = 'DEBIT CARD',
+    BILLET = 'BILLET'
+}
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+import * as nr from 'newrelic'
+import { PaymentMethod } from './domain/enums'
+
+
+nr.recordMetric('Cart/ChargeAmount', 29.99)
+
+nr.recordCustomEvent('Cart_Purchase', {
+    TotalValue: 29.99,
+    ItemQuantity: 3,
+    CartTimeDuration: 600,
+    PaymentMethod: PaymentMethod.CREDIT_CARD,
+    Installments: 1
+})
+
+console.log('funciona!!')
